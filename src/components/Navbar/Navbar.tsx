336aec1443243f267e9233db4d455e5a7f920d1c
Dmytro Farbun <dimofarb@gmail.com>
2026-05-05 20:20:12 +0300

    fix(Navbar): ensure active state indicator for navigation items

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,28 +1,29 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { PATHS } from "../../variables";
-
-import css from "./Navbar.module.css";
 import { useAuth } from "../../hooks/useAuth";
+import css from "./Navbar.module.css";
 
 export default function NavBar() {
   const { user } = useAuth();
+  const { pathname } = useLocation();
+  const currentPath = pathname.startsWith("/") ? pathname.slice(1) : pathname;
 
   return (
     <nav className={css.navbar}>
       <ul className={css.navList}>
-        <li className={css.navListItem}>
-          <Link className={css.navLink} to={PATHS.home}>
+        <li className={`${css.navListItem}`}>
+          <Link className={css.navLink} to={`/${PATHS.home}`}>
             Home
           </Link>
         </li>
-        <li className={css.navListItem}>
-          <Link className={css.navLink} to={PATHS.psychologists}>
+        <li className={`${css.navListItem} ${currentPath === PATHS.psychologists ? css.active : ""}`}>
+          <Link className={css.navLink} to={`/${PATHS.psychologists}`}>
             Psychologists
           </Link>
         </li>
         {user && (
-          <li className={css.navListItem}>
-            <Link className={css.navLink} to={PATHS.favourites}>
+          <li className={`${css.navListItem} ${currentPath === PATHS.favourites ? css.active : ""}`}>
+            <Link className={css.navLink} to={`/${PATHS.favourites}`}>
               Favourites
             </Link>
           </li>
